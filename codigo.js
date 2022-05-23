3d37b95d7bf9796990a93b0eae0359aafc5a44ca
zumsteind <zumsteind@gmail.com>
2022-05-22 23:51:47 -0300

    sitio web formulaBurguer

---
+++ codigo.js
@@ -0,0 +1,17 @@
+
+
+
+function total(){
+
+
+ 
+ var uno=850* parseInt ( document.getElementById("uno").value*1);
+ var dos=910* parseInt (document.getElementById("dos").value*1);
+ var tres=1070* parseInt (document.getElementById("tres").value*1);
+
+ 
+var total=uno+dos+tres;
+console.log("EL TOTAL ES "+total);
+alert("TOTAL A PAGAR:"+total+"$");
+
+}
